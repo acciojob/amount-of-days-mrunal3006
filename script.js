c8cf8b4a3fc4302af6efdd6bfd46092df689455a
mrunal3006 <128721188+mrunal3006@users.noreply.github.com>
2023-12-01 13:27:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-//your JS code here. If required.
 function daysOfAYear(year) {
-    // Check if the year is a leap year
-    const isLeapYear = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
-
-    // Return the number of days based on leap year or not
-    return isLeapYear ? 366 : 365;
+  // Check if the year is a leap year
+  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    return 366; // Leap year has 366 days
+  } else {
+    return 365; // Non-leap year has 365 days
+  }
 }
 
 // Examples
 console.log(daysOfAYear(2022)); // Output: 365
-console.log(daysOfAYear(2024)); // Output: 366
-console.log(daysOfAYear(1900)); // Output: 365
-console.log(daysOfAYear(2000)); // Output: 366
